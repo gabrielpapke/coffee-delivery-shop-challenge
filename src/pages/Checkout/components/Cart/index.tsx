baa3423c9c650f374345c75e5d0d977a69cf22ca
Gabriel Papke Paganini <gabrielpapkepaganini@192.168.0.10>
2023-06-01 19:42:01 -0300

    feat: checkout style

--- src/pages/Checkout/components/Cart/index.tsx
+++ src/pages/Checkout/components/Cart/index.tsx
@@ -1,5 +1,67 @@
-import { CartContainer } from './styles'
+import { Trash } from '@phosphor-icons/react'
+import { InputCounter } from '../../../../components/InputCounter'
+import expressoImg from '../../../../assets/images/coffee-types/expresso.svg'
+import { CartContainer, CartListContainer } from './styles'
 
 export function Cart() {
-  return <CartContainer>Carrinho</CartContainer>
+  return (
+    <CartContainer>
+      <CartListContainer>
+        <li>
+          <img src={expressoImg} alt="" />
+
+          <div>
+            <span className="product-name">Expresso Tradicional</span>
+
+            <div className="cart-options">
+              <InputCounter inputSize="small" value={1} />
+
+              <button className="remove" type="button">
+                <Trash weight="regular" size={16} />
+                Remover
+              </button>
+            </div>
+          </div>
+          <span className="price">R$ 19,90</span>
+        </li>
+
+        <li>
+          <img src={expressoImg} alt="" />
+
+          <div>
+            <span className="product-name">Expresso Tradicional</span>
+
+            <div className="cart-options">
+              <InputCounter inputSize="small" value={1} />
+
+              <button className="remove" type="button">
+                <Trash weight="regular" size={16} />
+                Remover
+              </button>
+            </div>
+          </div>
+          <span className="price">R$ 19,90</span>
+        </li>
+      </CartListContainer>
+
+      <div className="cart-totals">
+        <div>
+          <small>Total de itens</small>
+          <span>R$ 29,70</span>
+        </div>
+
+        <div>
+          <small>Entrega</small>
+          <span>R$ 3,50</span>
+        </div>
+
+        <div>
+          <strong>Total</strong>
+          <strong>R$ 33,20</strong>
+        </div>
+      </div>
+
+      <button type="submit">Confirmar pedido</button>
+    </CartContainer>
+  )
 }
--- src/pages/Checkout/components/Cart/styles.ts
+++ src/pages/Checkout/components/Cart/styles.ts
@@ -4,4 +4,104 @@ import { Card } from '../../styles'
 export const CartContainer = styled(Card)`
   border-bottom-left-radius: 44px;
   border-top-right-radius: 44px;
+
+  .cart-totals {
+    padding-bottom: 1.5rem;
+  }
+
+  .cart-totals div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    & + div {
+      margin-top: 0.75rem;
+    }
+
+    small {
+      font-size: 0.875rem;
+    }
+    span {
+    }
+    strong {
+      font-size: 1.25rem;
+    }
+  }
+
+  button[type='submit'] {
+    background-color: ${(props) => props.theme.yellow};
+    color: ${(props) => props.theme.white};
+    display: flex;
+    width: 100%;
+    text-transform: uppercase;
+    justify-content: center;
+    align-items: center;
+    border: 0;
+    border-radius: 6px;
+    font-size: 0.875rem;
+    font-weight: 700;
+    height: 2.5rem;
+    cursor: pointer;
+
+    &:hover {
+      background-color: ${(props) => props.theme['yellow-dark']};
+    }
+  }
+`
+
+export const CartListContainer = styled.ul`
+  list-style: none;
+
+  img {
+    max-width: 100%;
+  }
+  li {
+    list-style: none;
+    padding: 0.5rem 0.25rem;
+    margin-bottom: 1.5rem;
+
+    display: grid;
+    grid-template-columns: 65px 1fr 70px;
+    column-gap: 1.25rem;
+    padding-bottom: 1.5rem;
+    border-bottom: 1px solid ${(props) => props.theme['base-button']};
+  }
+
+  .product-name {
+    color: ${(props) => props.theme['base-subtitle']};
+  }
+
+  .cart-options {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+
+    padding-top: 0.5rem;
+  }
+
+  .remove {
+    height: 2rem;
+    border-radius: 6px;
+    background-color: ${(props) => props.theme['base-button']};
+    border: 0;
+    display: flex;
+    align-items: center;
+    padding: 0 0.5rem;
+    text-transform: uppercase;
+    font-size: 0.75rem;
+    gap: 0.25rem;
+    cursor: pointer;
+
+    &:hover {
+      background-color: ${(props) => props.theme['base-hover']};
+    }
+
+    svg {
+      color: ${(props) => props.theme.purple};
+    }
+  }
+
+  .price {
+    font-weight: 700;
+  }
 `
